10eaa33dffb04a35f99f897823b5d3bd435327b2
Brody Hicks <BHicks7@my.gcu.edu>
2022-04-01 16:20:15 -0700

    Updated comments & added user-defined function and sum arrary to source code.

--- Calculator.c
+++ Calculator.c
@@ -1,29 +1,35 @@
 /*
 Filename: ITT-310 C Calculator
 Author: Brody Hicks
-Date: 3/25/2022
-Version: 1.0
+Date: 4/1/2022
+Version: 2.0
 Purpose for release: This release of the calculator will allow for a multifunctional
 interpretation of user input and the choice of mathematica operations including (+,-,* and /)
+In addition to the use of an arrary for the sum of 10 numbers and modularizing the code from the main 
+as a user-defined function to be called from the main program
 */
+#include <stdio.h> // Main C library preprocessor header file
+#include <errno.h>
 
-#include <stdio.h>
+void calc(); // Define my modularity for the user-defined function
  
-int main() //Main function start of the program
+void calc() //Main function start of the program
 {
 	int a , b,choice; //Declaring integer variables and data types, as well as choice for decision
-	int d = 1; //Declare value to 1 for loop
+	int c[10], i;
+	int s = 0;
 
-	while (d == 1) { // While loop used for trying again once the program is complete
+	{ // While loop used for trying again once the program is complete
 
 		printf("Please enter your first number: "); //Asks user to input first integer and sets to memory
 		scanf_s("%d", &a);
 
 		printf("Please enter your second number: "); //Asks user for second number and sets to memory
 		scanf_s("%d", &b);
-		
 
-		printf("\nMultiply(1), Divide(2), Subract(3), Add(4): "); //Prompts user to choose an operator to perform calculation
+
+		printf("\nMultiply(1), Divide(2), Subract(3), Add(4), Sum(5): "); //Prompts user to choose an operator to perform calculation
+		// with the addition of the arrary for the sum of 10 numbers
 		scanf_s("%d", &choice);
 
 		switch (choice) //decision switch choice
@@ -40,11 +46,28 @@ int main() //Main function start of the program
 		case(4):
 			printf("%d + %d = %d\n", a, b, (a + b)); //Will add 2 numbers inputted
 			break;
-		default:
-			printf("\nThat is an invalid choice, that is not a displayed options!!!!! :)"); //default statement if error occurs for case input value
+		case(5):
+			for (i = 0; i < 10; i++) { // For loop for the array starting at 0, with incremental inputs of 10 digits to do the sum
+				printf("\nEnter a number"); //Enter 10 digits incrementally
+				scanf_s("%d", &c[i]); //Store digits from the array as integers
+				s = s + c[i]; //Perform the mathematical sum operation of all 10 integers
+			}
+			printf("\nSum = %d", s); //Display the sum as "Sum =     "
 		}
-		printf("\nWould you like to try again (1 for yes 2 for no): "); //Prompts user to start again or leave the program (1/Y or 2/N)
-		scanf_s("%d", &d);
+	
 	}
-		return 0; //End of program
+}
+// Main function for the program for the execution and calling of the defined function for the calculator
+int main() {
+	//Define my loop variable set to 1
+	int d = 1;
+	calc(); // Call the user-defined function
+	while (d == 1) { // While loop for the recursive operation for the user to restart the program
+		printf("\nWould you like to try again (1 for yes, all other numbers for No): ");
+		scanf_s("%d", &d);
+		if (d == 1) { // If user inputs #1 1 the program will restart, else the program will end.
+			calc(); //If selected 1, recall the user-defined function 
+		}
+	}
+	return 0; //Else, the program will end
 }
