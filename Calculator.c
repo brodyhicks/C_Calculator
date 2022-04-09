e41d4e0d0c58ea02fdd6c4839fcc7c0569b56c89
Brody Hicks <BHicks7@my.gcu.edu>
2022-04-08 17:16:02 -0700

    Added union and structures 4/8/2022

--- Calculator.c
+++ Calculator.c
@@ -27,24 +27,36 @@ void calc() //Main function start of the program
 		int day; //Defined day integer
 		int year; //Defined year integers
 	}Date; //Name of the structure
+	typedef struct 
+	{
+		int hour;
+		int minute;
+	}time;
 
 	//Create an instance of the date today for the structure
 	Date today;
+	time now;
 
 	//Populate the Date Structure with the integers for the month, day and year specified as 04/8/2022
 	today.month = 04;
 	today.day = 8;
 	today.year = 2022;
 
+	now.hour = 5;
+	now.minute = 9;
+
 	union Data data; //Call the union for data
 	data.version = 1.03; //Define the union data version to be displayed
 	printf("version : %1.2f\n", data.version); //Print the Version as 1.03
 	strcpy(data.string, "Brody Rantz Hicks"); //Copy the character string to be my name as the developer
 	printf("Developed by : %s\n", data.string); //displays the character string as my full name Brody Rantz Hicks
+	strcpy(data.string, "Welcome to the best calculator for students to learn math operations!!!");
+	printf("General info:  %s\n", data.string);
 
 	// Print the contents of the Data Structure as the full date today
 	printf_s("The date today is %i/%i/%.2i.\n", today.month, today.day, today.year % 100);
-
+	printf_s("The time this was made was %01d:%02d pm\n\n",now.hour, now.minute % 100);
+	
 	int a , b,choice; //Declaring integer variables and data types, as well as choice for decision
 	int c[10], i;
 	int s = 0;
@@ -92,8 +104,7 @@ int main() {
 	//Define my loop variable set to 1
 	int d = 1;
 	calc(); // Call the user-defined function
-	while (d == 
-		) { // While loop for the recursive operation for the user to restart the program
+	while (d == 1) { // While loop for the recursive operation for the user to restart the program
 		printf("\nWould you like to try again (1 for yes, all other numbers for No): ");
 		scanf_s("%d", &d);
 		if (d == 1) { // If user inputs #1 1 the program will restart, else the program will end.
