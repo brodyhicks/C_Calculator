16729e8c685891bcae22fcfbc98b156e21af4fcd
Brody Hicks <BHicks7@my.gcu.edu>
2022-04-17 12:31:04 -0700

    Updated calculator source code for cleanup and debugging

--- Calculator.c
+++ Calculator.c
@@ -8,16 +8,35 @@ defined and display the version of the calculator, as well as a character string
 the structure will be used as a record for storing and displaying today's current date for day, month and year and display
 it to the console at the top as a header introduction.
 */
+#include <stdlib.h>
 #include <stdio.h> // Main C library preprocessor header file
 #define _CRT_SECURE_NO_WARNINGS //The use of secure versions for library functions
+#include <ctype.h>
+#include <errno.h>
+#include <time.h>
+#include <Windows.h>
 
 void calc(); // Define my modularity for the user-defined function
 
-void calc() //Main function start of the program
+void delay(int number_of_seconds)
 {
+	// Converting time into milli_seconds
+	int milli_seconds = 1000 * number_of_seconds;
+
+	// Storing start time
+	clock_t start_time = clock();
+
+	// looping till required time is not achieved
+	while (clock() < start_time + milli_seconds);
+}
+
+void calc() //Main function start of the program
+{ 
+	delay(2);
+	system("cls");
 	union Data { //Union to display general data information for the version and a defined character string to be displayed
 		float version;
-		char string[100];
+		char name[100];
 	};
 
 	// A typedef data structure called date to display the current month, day and year as an official date of the program as a header description  
@@ -49,33 +68,46 @@ void calc() //Main function start of the program
 	union Data data; //Call the union for data
 	data.version = 1.03; //Define the union data version to be displayed
 	printf("version : %1.2f\n", data.version); //Print the Version as 1.03
-	strcpy(data.string, "Brody Rantz Hicks"); //Copy the character string to be my name as the developer
-	printf("Developed by : %s\n", data.string); //displays the character string as my full name Brody Rantz Hicks
-	strcpy(data.string, "Welcome to the best calculator for students to learn math operations!!!"); //Copythe data string as 
+
+	strcpy_s(data.name, sizeof data.name, "Brody Rantz Hicks"); //Copy the character string to be my name as the developer
+	printf("Developed by : %s\n", data.name); //displays the character string as my full name Brody Rantz Hicks
+	strcpy_s(data.name, sizeof data.name, "Welcome to the best calculator for students to learn math operations!!!"); //Copythe data string as 
 	//the welcome to the calculator
-	printf("General info:  %s\n", data.string); //Display the character string as the welcome banner to the calculator
+	printf("General info:  %s\n", data.name); //Display the character string as the welcome banner to the calculator
 
 	// Print the contents of the Data Structure as the full date today, as well as the time
-	printf_s("The date today is %i/%i/%.2i.\n", today.month, today.day, today.year % 100);
-	printf_s("The time this was made was %01d:%02d pm\n\n",now.hour, now.minute % 100);
+	printf("The date today is %i/%i/%.2i.\n", today.month, today.day, today.year % 100);
+	printf("The time this was made was %01d:%02d pm\n\n",now.hour, now.minute % 100);
 	
-	int a , b,choice; //Declaring integer variables and data types, as well as choice for decision
+	int a, b, choice;//Declaring integer variables and data types, as well as choice for decision
 	int c[10], i;
 	int s = 0;
 
 	{ // While loop used for trying again once the program is complete
 
-		printf("Please enter your first number: "); //Asks user to input first integer and sets to memory
+		printf("Please enter your first number: \n"); //Asks user to input first integer and sets to memory
 		scanf_s("%d", &a);
-
-		printf("Please enter your second number: "); //Asks user for second number and sets to memory
+		if (a == 0) {
+			printf("Thats boring, pick something other then 0, jeeeez haha!!\n");
+			printf("This is an invalid choice!!You can't do any math with 0\n");
+			calc();
+		}
+		printf("Please enter your second number: \n"); //Asks user for second number and sets to memory
 		scanf_s("%d", &b);
 
-
+		if (b == 0) {
+			printf("Thats boring, pick something other then 0, jeeeez haha!!\n");
+			printf("This is an invalid choice!!You can't do any math with 0\n");
+			calc();
+		}
 		printf("\nMultiply(1), Divide(2), Subract(3), Add(4), Sum(5):  \n"); //Prompts user to choose an operator to perform calculation
 		// with the addition of the arrary for the sum of 10 numbers
 		scanf_s("%d", &choice);
 
+		if (choice > 5) {
+			printf("This is an invalid choice!!Must input a value between 1-5\n");
+			calc();
+		}
 		switch (choice) //decision switch choice
 		{
 		case(1):
@@ -97,6 +129,9 @@ void calc() //Main function start of the program
 				s = s + c[i]; //Perform the mathematical sum operation of all 10 integers
 			}
 			printf("\nSum = %d", s); //Display the sum as "Sum =     "
+		default:
+			printf("Please input a valid choice from the list\n");
+
 		}
 	
 	}
